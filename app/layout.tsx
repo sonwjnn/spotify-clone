6de4e5c62567b7a0039ce7d283ea44459233d60a
sonwjnn <hoangsonj4f@gmail.com>
2023-09-01 10:35:01 +0700

    create favorite

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,30 +5,35 @@ import SupabaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
 import ToasterProvider from '@/providers/ToasterProvider'
+import getSongsByUserId from '@/actions/getSongsByUserId'
 
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Spotify Clone',
-  description: 'Listen to music!'
+	title: 'Spotify Clone',
+	description: 'Listen to music!',
 }
 
-export default function RootLayout({
-  children
+export const revalidate = 0
+
+export default async function RootLayout({
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={font.className}>
-        <ToasterProvider />
-        <SupabaseProvider>
-          <UserProvider>
-            <ModalProvider />
-            <Sidebar>{children}</Sidebar>
-          </UserProvider>
-        </SupabaseProvider>
-      </body>
-    </html>
-  )
+	const userSongs = await getSongsByUserId()
+
+	return (
+		<html lang='en'>
+			<body className={font.className}>
+				<ToasterProvider />
+				<SupabaseProvider>
+					<UserProvider>
+						<ModalProvider />
+						<Sidebar songs={userSongs}>{children}</Sidebar>
+					</UserProvider>
+				</SupabaseProvider>
+			</body>
+		</html>
+	)
 }
