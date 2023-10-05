1141a25ff9a0f8794a27c3176711c07bb18a76cb
sonwjnn <hoangsonj4f@gmail.com>
2023-10-05 18:14:30 +0700

    liked select

--- components/Library.tsx
+++ components/Library.tsx
@@ -5,6 +5,9 @@ import { LibraryIcon } from "@/public/icons";
 import UploadDropdown from "./UploadDropdown";
 import PlaylistSidebar from "./PlaylistSidebar/PlaylistSidebar";
 import { Playlist } from "@/types";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useSubscribeModal from "@/hooks/useSubscribeModal";
 
 interface LibraryProps {
   playlists: Playlist[];
@@ -12,8 +15,20 @@ interface LibraryProps {
 }
 
 const Library: React.FC<LibraryProps> = ({ playlists, isScroll = false }) => {
+  const { user, subscription } = useUser();
+  const authModal = useAuthModal();
+  const subcribeModal = useSubscribeModal();
+
+  const handleClick = () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+    if (!subscription) {
+      return subcribeModal.onOpen();
+    }
+  };
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col ">
       <div
         className={`flex flex-col items-center px-5 pt-4 sticky top-0 bg-neutral-900 z-10 pb-0 ${
           isScroll ? "shadow-2xl" : ""
@@ -33,7 +48,10 @@ const Library: React.FC<LibraryProps> = ({ playlists, isScroll = false }) => {
         </div>
 
         <div className="h-12 w-full flex items-center gap-x-2 mt-2">
-          <button className=" rounded-full bg-neutral-800 text-white text-sm border border-transparent py-1 px-3 disabled:cursor-not-allowed disabled:opacity-50 transition hover:brightness-110">
+          <button
+            disabled={playlists.length ? false : true}
+            className="rounded-full bg-neutral-800 text-white text-sm border border-transparent py-1 px-3  disabled:opacity-50 transition hover:brightness-110"
+          >
             Playlists
           </button>
           <button
@@ -44,7 +62,16 @@ const Library: React.FC<LibraryProps> = ({ playlists, isScroll = false }) => {
           </button>
         </div>
       </div>
-      <PlaylistSidebar data={playlists} />
+      {playlists.length ? (
+        <PlaylistSidebar data={playlists} />
+      ) : (
+        <div
+          onClick={handleClick}
+          className="flex justify-center items-center text-center w-full px-4 mt-8 text-neutral-400 hover:text-white transition cursor-pointer"
+        >
+          Log in and subscribe to view your playlists.
+        </div>
+      )}
     </div>
   );
 };
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { useUser } from "@/hooks/useUser";
 import { MusicNote } from "@/public/icons";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 interface ListItemProps {
   image: string;
@@ -12,38 +13,46 @@ interface ListItemProps {
 }
 
 const ListItem: React.FC<ListItemProps> = ({ image, name, href, count }) => {
+  const { user } = useUser();
   const router = useRouter();
+  const pathName = usePathname();
 
   const onClick = () => {
     router.push(href);
   };
 
   return (
-    <div
-      className={` cursor-pointer rounded-md p-2 transition w-full hover:bg-neutral-800/50 `}
-    >
-      <div onClick={onClick} className="flex item-center gap-x-3">
-        <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
-          {image ? (
-            <Image
-              fill
-              src={image}
-              sizes="100%"
-              alt="Media-Item"
-              className="object-cover"
-            />
-          ) : (
-            <div className="w-full h-full flex items-center justify-center bg-neutral-800">
-              <MusicNote size={20} />
+    <>
+      {user ? (
+        <div
+          className={` cursor-pointer rounded-md p-2 transition w-full hover:bg-neutral-800/50 ${
+            pathName.includes("/liked") && "bg-neutral-800/50"
+          }`}
+        >
+          <div onClick={onClick} className="flex item-center gap-x-3">
+            <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
+              {image ? (
+                <Image
+                  fill
+                  src={image}
+                  sizes="100%"
+                  alt="Media-Item"
+                  className="object-cover"
+                />
+              ) : (
+                <div className="w-full h-full flex items-center justify-center bg-neutral-800">
+                  <MusicNote size={20} />
+                </div>
+              )}
             </div>
-          )}
+            <div className="flex flex-col gap-y-1 overflow-hidden">
+              <p className="text-white truncate">{name}</p>
+              <p className="text-neutral-400 text-sm truncate">{`Playlist - ${count} songs`}</p>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-y-1 overflow-hidden">
-          <p className="text-white truncate">{name}</p>
-          <p className="text-neutral-400 text-sm truncate">{`Playlist - ${count} songs`}</p>
-        </div>
-      </div>
-    </div>
+      ) : null}
+    </>
   );
 };
 
