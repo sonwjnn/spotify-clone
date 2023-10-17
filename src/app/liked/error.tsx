c8ec83c6d18dbaa3d7f17c4589502eabe89c4297
sonwjnn <hoangsonj4f@gmail.com>
2023-10-17 21:38:21 +0700

    fix file problem

--- src/app/liked/error.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { Alert } from '@/components/alert'
-
-const Error: React.FC = () => {
-  return <Alert type="wrong" />
-}
-
-export default Error
--- src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { Alert } from '@/components/alert'
-
-const notFoundPage: React.FC = () => {
-  return <Alert type="notfound" />
-}
-
-export default notFoundPage
